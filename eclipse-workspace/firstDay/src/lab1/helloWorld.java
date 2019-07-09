17a8ae8026ac4747567a45e006fd78b0b1068fcb
Octavia-White <52667326+Octavia-White@users.noreply.github.com>
2019-07-09 13:08:40 -0400

    Add files via upload

---
+++ eclipse-workspace/firstDay/src/lab1/helloWorld.java
@@ -0,0 +1,23 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello World" );
+	Scanner scnr = new Scanner (System.in);		
+	System.out.println("write your name");
+	
+	System.out.println("how many siblings do you have?" );
+	
+	System.out.println("Thank you for your input !");
+	
+	}
+	
+	
+	
+	
+	
+}
---
+++ eclipse-workspace/practice/src/bio/aboutMe.java
@@ -0,0 +1,12 @@
+package bio;
+
+public class aboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("name;Octavia");
+System.out.println("15");
+System.out.println("11th grade");
+	}  
+
+}
